a2376ad90fa132ea61d51692b79ae5d3ac340274
dam <dam@203P29>
2024-02-22 09:46:16 +0100

    Lineas del metodo main

--- src/ejercitiogit/EjercitioGit.java
+++ src/ejercitiogit/EjercitioGit.java
@@ -14,7 +14,8 @@ public class EjercitioGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        System.out.println("Linea 1");
+        System.out.println("Linea 2");
     }
     
 }
