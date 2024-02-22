6e6606f39a8c6fa56d0e27281f1a514aacdc09f1
dam <dam@203P29>
2024-02-22 09:52:45 +0100

    Se ha creado la rama clasenueva.

---
+++ src/ejercitiogit/clasenueva.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercitiogit;
+
+/**
+ *
+ * @author dam
+ */
+public class clasenueva {
+      public static void main(String[] args) {
+        System.out.println("Se ha creado la rama clase nueva.");
+      }
+}
